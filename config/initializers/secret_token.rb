6093185fedbce15c53b5b592d4b12e5a3870aa15
aschmi <tdimhcs.nairda@gmail.com>
2014-03-21 17:46:29 +0100

    Add initial commit to repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ObliviousMe::Application.config.secret_key_base = 'f8fc56fd4507b683d2a38fa41d73177852a01c16f5cf53b19a9c0ed6428ac86c222a88f33260ffed11260185067e4cf3719bc682b75e738380f95395720376c1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ObliviousMe::Application.config.session_store :cookie_store, key: '_oblivious-me_session'
